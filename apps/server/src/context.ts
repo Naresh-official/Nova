075c01df100976fc81dbe48516e398426a376db4
Naresh-official <naresh01.official@gmail.com>
2025-07-09 20:01:05 +0530

    update : update controller to hash refresh token before saving in database

--- apps/server/src/context.ts
+++ apps/server/src/context.ts
@@ -1,11 +1,15 @@
-import prisma from "@nova/db";
 import type { CreateExpressContextOptions } from "@trpc/server/adapters/express";
+import prisma from "@nova/db";
+import bcrypt from "bcryptjs";
 
 export function createContext({ req, res }: CreateExpressContextOptions) {
-  return {
-    req,
-    res,
-    prisma: prisma,
-  };
+	return {
+		req,
+		res,
+		prisma: prisma,
+		hashPassword: (password: string) => bcrypt.hash(password, 12),
+		verifyPassword: (password: string, hashedPassword: string) =>
+			bcrypt.compare(password, hashedPassword),
+	};
 }
 export type Context = ReturnType<typeof createContext>;
